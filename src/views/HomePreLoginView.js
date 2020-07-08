2ddc8df282527b11bc249aa9bc3b40cb25e39652
BigBinnie <838719915@qq.com>
2020-07-08 21:18:32 +0800

    feat: add sidebar

--- src/views/HomePreLoginView.js
+++ src/views/HomePreLoginView.js
@@ -1,7 +1,8 @@
 import React, {Component} from 'react';
 import {Layout} from "antd";
 import PrimarySearchAppBar from "../components/basic/Header";
-const { Header, Content, Footer } = Layout;
+import SideBar from "../components/basic/SideBar";
+const { Header, Footer, Sider, Content } = Layout;
 class HomePreLoginView extends Component{
 
     render() {
@@ -11,9 +12,11 @@ class HomePreLoginView extends Component{
                     <PrimarySearchAppBar/>
                 </Header>
                 <Layout>
+                    <Sider><SideBar/></Sider>
                     <Content style={{ padding: '0 50px' }}>
                         <div className="home-content">
                             <div className={"foot-wrapper"}>
+
                                 HomePreLogin page!
                             </div>
                         </div>
