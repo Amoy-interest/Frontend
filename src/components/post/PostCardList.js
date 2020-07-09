fe0c656685de4c67aedfeb897e9e71e1f5e7d7c0
BigBinnie <838719915@qq.com>
2020-07-09 23:10:34 +0800

    feat: add comment list

--- src/components/post/PostCardList.js
+++ src/components/post/PostCardList.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
+import List from '@material-ui/core/List';
 import { FixedSizeList } from 'react-window';
 import PostCard from "./PostCard";
 import {Divider} from "@material-ui/core";
@@ -11,36 +12,30 @@ const useStyles = makeStyles((theme) => ({
     root: {
         marginTop:theme.spacing(3),
         width: '100%',
-        // height: 700,
-        maxWidth: 450,
+        maxWidth: 485,
         backgroundColor: theme.palette.background.paper,
     },
 }));
-
-function renderRow(props) {
-    const { index, style } = props;
-
-    return (
-        <ListItem button style={style} key={index}>
-            <PostCard/>
-            <Divider/>
-        </ListItem>
-    );
-}
-
-renderRow.propTypes = {
-    index: PropTypes.number.isRequired,
-    style: PropTypes.object.isRequired,
-};
 
 export default function PostCardList() {
     const classes = useStyles();
 
     return (
         <div className={classes.root}>
-            <FixedSizeList height={700} width={450} itemSize={500} itemCount={10}>
-                {renderRow}
-            </FixedSizeList>
+            <List>
+                <ListItem button>
+                    <PostCard/>
+                    <Divider/>
+                </ListItem>
+                <ListItem button>
+                    <PostCard/>
+                    <Divider/>
+                </ListItem>
+                <ListItem button>
+                    <PostCard/>
+                    <Divider/>
+                </ListItem>
+            </List>
         </div>
     );
 }
