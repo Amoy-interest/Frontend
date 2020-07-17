afb7d469c26f0e47741842736473e2f62a231312
BigBinnie <838719915@qq.com>
2020-07-17 09:57:55 +0800

    feat: add Posts Service

--- src/routers/PrivateRouter.js
+++ src/routers/PrivateRouter.js
@@ -28,6 +28,7 @@ const useStyles = makeStyles((theme) =>
         },
         content: {
             flexGrow: 1,
+            padding: theme.spacing(3),
         },
         container: {
             minHeight: '85vh',
