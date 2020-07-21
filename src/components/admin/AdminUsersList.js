b04513498c2e774ee0a46d1177abfa1aef686db0
explodingnerk <wuzechuan@sjtu.edu.cn>
2020-07-21 23:53:58 +0800

    feat: implementing ban user and forbid user func

--- src/components/admin/AdminUsersList.js
+++ src/components/admin/AdminUsersList.js
@@ -16,6 +16,14 @@ import TableCell from "@material-ui/core/TableCell";
 import Tooltip from "@material-ui/core/Tooltip";
 import BlockIcon from "@material-ui/icons/Block";
 import DeleteIcon from "@material-ui/icons/Delete";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogActions from "@material-ui/core/DialogActions";
+import Dialog from "@material-ui/core/Dialog";
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
 
 const StyledTableCell = withStyles((theme) => ({
     head: {
@@ -48,7 +56,9 @@ export default class AdminUsersList extends Component {
             checked: [],
             open: [],
             creditsOrder: "desc",
-            checkAll: false
+            checkAll: false,
+            showBanDialog: false,
+            showForbidDialog: false
         };
     }
 
@@ -90,6 +100,22 @@ export default class AdminUsersList extends Component {
             checked: tmp
         });
     }
+
+    cancelBan = () => {
+        this.setState({showBanDialog: false});
+    };
+
+    confirmBan = () => {
+        this.setState({showBanDialog: false});
+    };
+
+    cancelForbid = () => {
+        this.setState({showForbidDialog: false});
+    };
+
+    confirmForbid = () => {
+        this.setState({showForbidDialog: false});
+    };
 
     render() {
         return (
@@ -138,12 +164,12 @@ export default class AdminUsersList extends Component {
                                                 低俗，暴力，色情
                                             </StyledTableCell>
                                             <StyledTableCell>
-                                                <Tooltip title={"屏蔽"}>
+                                                <Tooltip title={"禁言"} onClick={() => {this.setState({showBanDialog: true})}}>
                                                     <IconButton edge="end" aria-label="ban" style={{marginLeft: '8px'}}>
                                                         <BlockIcon/>
                                                     </IconButton>
                                                 </Tooltip>
-                                                <Tooltip title={"删除"}>
+                                                <Tooltip title={"封号"} onClick={() => {this.setState({showForbidDialog: true})}}>
                                                     <IconButton edge="end" aria-label="micoff">
                                                         <DeleteIcon/>
                                                     </IconButton>
@@ -155,6 +181,67 @@ export default class AdminUsersList extends Component {
                         </TableBody>
                     </Table>
                 </TableContainer>
+                <Dialog open={this.state.showBanDialog} aria-labelledby="form-dialog-title">
+                    <DialogTitle id="form-dialog-title">禁言</DialogTitle>
+                    <DialogContent>
+                        <DialogContentText>
+                            请选择禁言时长
+                        </DialogContentText>
+                        <List>
+                            <ListItem button>
+                                <ListItemText>1小时</ListItemText>
+                            </ListItem>
+                            <ListItem button>
+                                <ListItemText>12小时</ListItemText>
+                            </ListItem>
+                            <ListItem button>
+                                <ListItemText>1天</ListItemText>
+                            </ListItem>
+                            <ListItem button>
+                                <ListItemText>7天</ListItemText>
+                            </ListItem>
+                            <ListItem button>
+                                <ListItemText>一个月</ListItemText>
+                            </ListItem>
+                            <ListItem button>
+                                <ListItemText>三个月</ListItemText>
+                            </ListItem>
+                            <ListItem button>
+                                <ListItemText>半年</ListItemText>
+                            </ListItem>
+                            <ListItem button>
+                                <ListItemText>一年</ListItemText>
+                            </ListItem>
+                            <ListItem button>
+                                <ListItemText>十年</ListItemText>
+                            </ListItem>
+                        </List>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={this.cancelBan} color="primary">
+                            取消
+                        </Button>
+                        <Button onClick={this.confirmBan} color="primary">
+                            确认禁言
+                        </Button>
+                    </DialogActions>
+                </Dialog>
+                <Dialog open={this.state.showForbidDialog} aria-labelledby="form-dialog-title">
+                    <DialogTitle id="form-dialog-title">封号</DialogTitle>
+                    <DialogContent>
+                        <DialogContentText>
+                            请选择封号时长
+                        </DialogContentText>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={this.cancelForbid} color="primary">
+                            取消
+                        </Button>
+                        <Button onClick={this.confirmForbid} color="primary">
+                            确认封号
+                        </Button>
+                    </DialogActions>
+                </Dialog>
             </div>
         );
     }
